5d41109a68e6e5f1e531dbe31de3aa895c833ee7
Svetli779 <svetli.mvl@gmail.com>
2015-05-17 19:22:36 +0300

    finaly homework

--- homewor06/links.php
+++ homewor06/links.php
@@ -6,13 +6,20 @@
     </head>
     <body>
         <?php
-       $cars= array('BMW'=>'bmw.com/com/en/','FERRARI'=>'ferrari.com/bg_bg/',
+  $cars= array('BMW'=>'bmw.com/com/en/','FERRARI'=>'ferrari.com/bg_bg/',
             'lotus'=>'lotuscars.com/','lamborghini'=>'lamborghini.com/en/home/',
             'Porshe'=>'porsche.com/');
-       foreach ($cars as $key=>$value) {
-           echo "<a href='http://www.$value'>$key</a><br/>";
-       }
 
-        ?>
+  $keys = array_keys($cars); 
+  shuffle($keys); 
+  $random = array(); 
+  foreach ($keys as $key) { 
+        $random[$key] = $cars[$key]; 
+ 
+  }
+  foreach($random as $key=>$value){
+         echo "<a href='http://www.$value'>$key</a><br/>";
+  }
+         ?>
     </body>
 </html>
