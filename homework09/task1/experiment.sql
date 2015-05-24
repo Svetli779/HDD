57ca698b7cb35d3eb7fbe864df75afd7b6192813
Svetli779 <svetli.mvl@gmail.com>
2015-05-24 22:38:38 +0300

    task1

---
+++ homework09/task1/experiment.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2015 at 09:36 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `experiment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE IF NOT EXISTS `brands` (
+  `brands_id` int(12) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brands_id`, `name`) VALUES
+(1, 'Ford'),
+(2, 'Lotus'),
+(3, 'Porshe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE IF NOT EXISTS `cars` (
+  `cars_id` int(12) NOT NULL,
+  `color` varchar(32) NOT NULL,
+  `year` date NOT NULL,
+  `brands_id` int(11) NOT NULL,
+  `region_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`cars_id`, `color`, `year`, `brands_id`, `region_id`) VALUES
+(1, 'red', '2012-12-12', 1, 1),
+(2, 'green', '2014-01-01', 2, 2),
+(3, 'red', '2015-05-05', 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regions`
+--
+
+CREATE TABLE IF NOT EXISTS `regions` (
+  `region_id` int(12) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `country` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `regions`
+--
+
+INSERT INTO `regions` (`region_id`, `name`, `country`) VALUES
+(1, 'Sofia', 'Bulgaria'),
+(2, 'Varna', 'Bulgaria'),
+(3, 'Paris', 'France');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brands_id`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`cars_id`), ADD UNIQUE KEY `brands_id` (`brands_id`), ADD UNIQUE KEY `region_id` (`region_id`), ADD UNIQUE KEY `region_id_2` (`region_id`);
+
+--
+-- Indexes for table `regions`
+--
+ALTER TABLE `regions`
+  ADD PRIMARY KEY (`region_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brands_id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `cars_id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `regions`
+--
+ALTER TABLE `regions`
+  MODIFY `region_id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cars`
+--
+ALTER TABLE `cars`
+ADD CONSTRAINT `cars` FOREIGN KEY (`brands_id`) REFERENCES `brands` (`brands_id`),
+ADD CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`region_id`) REFERENCES `regions` (`region_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
